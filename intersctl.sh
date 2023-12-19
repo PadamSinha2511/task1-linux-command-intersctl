c0665301b594c60b2a4816fbde47327f6133b956
PadamSinha2511 <padamsinha3@gmail.com>
2023-12-19 03:26:53 -0800

    final code done

--- intersctl.sh
+++ intersctl.sh
@@ -64,6 +64,38 @@ get_cpu_info() {
     lscpu
 }
 
+get_memory_info() {
+    free -h
+}
+
+create_user() {
+    if [ -z "$1" ]; then
+        echo "Error: Missing username. Usage: internsctl user create <username>"
+        exit 1
+    fi
+
+    username="$1"
+
+    # Check if the user already exists
+    if id "$username" &>/dev/null; then
+        echo "Error: User '$username' already exists."
+        exit 1
+    fi
+
+    # Create the user with a home directory
+    useradd -m -s /bin/bash "$username"
+
+    echo "User '$username' created successfully."
+}
+
+list_users() {
+    if [ "$1" == "--sudo-only" ]; then
+        getent group sudo | cut -d: -f4 | tr ',' '\n'
+    else
+        getent passwd | cut -d: -f1
+    fi
+}
+
 # Main script logic
 case "$1" in
     --help)
@@ -72,7 +104,6 @@ case "$1" in
     --version)
         echo "internsctl $VERSION"
         ;;
-
     cpu)
         case "$2" in
             getinfo)
@@ -84,11 +115,35 @@ case "$1" in
                 ;;
         esac
         ;;
+    memory)
+        case "$2" in
+            getinfo)
+                get_memory_info
+                ;;
+            *)
+                echo "Error: Unknown subcommand. Use 'internsctl memory getinfo' for memory information."
+                exit 1
+                ;;
+        esac
+        ;;
+    user)
+        case "$2" in
+            create)
+                create_user "$3"
+                ;;
+            list)
+                list_users "$4"
+                ;;
+            *)
+                echo "Error: Unknown subcommand. Use 'internsctl user create <username>' to create a user, 'internsctl user list' to list all users, or 'internsctl user list --sudo-only' to list users with sudo permissions."
+                exit 1
+                ;;
+        esac
+        ;;
     *)
-        echo "Error: Unknown option. Use 'internsctl --help' for usage instructions."
+        echo "Error: Unknown command. Use 'internsctl user create <username>' to create a user."
         exit 1
         ;;
-
 esac
 
 # Create the manual page
